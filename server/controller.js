4b1f6d1674af8b5809c1c1f46af66e10e31d083c
Senad Zenkovic <zenkovic88@gmail.com>
2021-11-30 15:48:47 -0700

    initial commit

---
+++ server/controller.js
@@ -0,0 +1,19 @@
+const foods = require('./db.json')
+
+module.exports = {
+    getFoods: (req, res) =>{
+
+    },
+
+    deleteFood: (req,res) =>{
+
+    },
+
+    updateFood: (req, res) =>{
+
+    },
+    
+    createFood: (req, res)=>{
+
+    }
+}
